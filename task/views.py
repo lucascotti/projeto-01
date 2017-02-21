f0d1386ff55ac18fc459a15728da995c76193118
Linguagem de Programação Comercial <lpc.pmw@labin.canoas>
2017-02-20 21:13:23 -0300

    primero commit

---
+++ task/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+class tarefa(object):
+    def __init__(self,titulo,data):
+        self.titulo=titulo
+        self.data=data
+    def __str__(self):
+        return self.titulo
+def home(request):
+    return HttpResponse("Ola")
+def sobre(request):
+    return HttpResponse("Lucas Scotti")
+#def tarefa(request,numero):
+    #return HttpResponse("tarefa:"+str(numero))
+def tarefa(request,ano,mes,dia):
+    return HttpResponse("tarefa:"+str(ano)+"/"+str(mes)+"/"+str(dia))
+
+# Create your views here.
